5e09b5fa13cc3e10bff72d1a4427568a81608b2e
lj970926 <1783973490@qq.com>
2026-04-23 12:46:28 +0800

    add args parse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::env;
+use std::fs;
+
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let (query, path) = parse_args(&args);
+    println!("File path: {file_path}");
+    let contents = fs::read_to_string(file_path).expect("Failed to open file.");
+    println!("Read content:\n {contents}");
 }
